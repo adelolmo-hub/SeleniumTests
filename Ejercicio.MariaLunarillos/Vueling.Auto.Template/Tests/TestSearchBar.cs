c41f09f49fa0fac36009f05091bb2a61dcc9f838
Alberto del Olmo Ventura <albertoov@in2.es>
2023-06-07 12:18:02 +0200

    first commit

---
+++ Ejercicio.MariaLunarillos/Vueling.Auto.Template/Tests/TestSearchBar.cs
@@ -0,0 +1,23 @@
+using AventStack.ExtentReports;
+using NUnit.Framework;
+using MariaLunarillos.WebPages;
+using System;
+using MariaLunarillos.Auto.Template.Common;
+using System.Threading;
+
+namespace MariaLunarillos.Auto.Template.Tests
+{
+    [TestFixture]
+    class TestSearchBar : TestSetCleanBase
+    {
+        [TestCase(1)]
+        public void test(int scenario)
+        {
+            
+            
+        }
+        
+
+    }
+
+}
---
+++ Ejercicio.MariaLunarillos/Vueling.Auto.Template/WebPages/HomePageShop.cs
@@ -0,0 +1,25 @@
+﻿using MariaLunarillos.Auto.Template.SetUp;
+using MariaLunarillos.Auto.Template.WebPages.Base;
+using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using System;
+using MariaLunarillos.Auto.Template.Common;
+using NUnit.Framework;
+
+namespace MariaLunarillos.WebPages
+{
+    public class HomePageShop : CommonPage
+    {
+        public HomePageShop(ISetUpWebDriver setUpWebDriver) : base(setUpWebDriver)
+        {
+        }
+        protected override IWebElement ApartadosBusqueda => throw new System.NotImplementedException();
+
+
+        
+        
+
+        
+    }
+}
